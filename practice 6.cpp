885e0de0b198656250c147d7e54a466fec4eb559
Rutvitickoo <122665411+Rutvitickoo@users.noreply.github.com>
2024-07-15 21:57:34 +0530

    Add files via upload

---
+++ practice 6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int inr;
+  cout<<"enter the amount inr" <<endl;
+  cin>>inr;
+  int pound =inr/106.90;
+  cout<<pound<<endl;
+  return 0;
+}

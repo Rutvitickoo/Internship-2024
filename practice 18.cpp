99fc00f6bd4877d68e30c8da835e12568a6ff237
Rutvitickoo <122665411+Rutvitickoo@users.noreply.github.com>
2024-07-15 22:06:07 +0530

    Add files via upload

---
+++ practice 18.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<String>
+using namespace std;
+int main(){
+std::string str=" welcome";
+int l=str.length();
+std::cout<<"length of string "<<l<<std::endl;
+
+std::cout<< str.at(3)<<std::endl;
+std.append("welcome back!");
+std::cout<< str;
+return 0;
+}
